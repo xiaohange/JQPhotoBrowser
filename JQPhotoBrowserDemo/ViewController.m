e9b428264ee829e777eeeb60e79294cdee15910b
HaRi <hjq99@163.com>
2017-08-10 10:12:27 +0800

    V1.0.0: 新增实例

--- JQPhotoBrowserDemo/ViewController.m
+++ JQPhotoBrowserDemo/ViewController.m
@@ -23,7 +23,15 @@
     [super viewDidLoad];
     self.view.backgroundColor = [UIColor whiteColor];
     
-    urlArray = @[@"http://cc.cocimg.com/api/uploads/170807/49eadc6d968c4f12b940773e75fed9bc.png",@"http://cc.cocimg.com/api/uploads/170809/0aba88dbca1f1e74c35897a35137c18d.png",@"http://cc.cocimg.com/api/uploads/170808/0698a822a848cdda0a24b9c2e75533d8.png",@"http://cc.cocimg.com/api/uploads/170804/59650af26689fe74bcc83bf211c9355f.png",@"http://cc.cocimg.com/api/uploads/170808/2c572f7ffc3a1935bbfe89439ede5940.png",@"http://cc.cocimg.com/api/uploads/20170809/1502261337401648.png",@"http://img4.07net01.com/upload/images/2017/03/25/55209250235451.gif",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502279410063&di=29202ef74ed4f488552a018c3455fba6&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01db52582566a5a84a0d304fa0ecc7.jpg",@"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502279258970&di=0414b6665f00c42ed92a4f19d3424e0f&imgtype=0&src=http%3A%2F%2Fimg.361games.com%2Ffile%2Ftu%2Fmeinv%2Fqbkrf43nnig.jpg"];
+    urlArray = @[@"http://cc.cocimg.com/api/uploads/170807/49eadc6d968c4f12b940773e75fed9bc.png",
+                 @"http://cc.cocimg.com/api/uploads/170809/0aba88dbca1f1e74c35897a35137c18d.png",
+                 @"http://cc.cocimg.com/api/uploads/170808/0698a822a848cdda0a24b9c2e75533d8.png",
+                 @"https://github.com/xiaohange/xiaohange.github.io/blob/master/images/csdnImg/test.png?raw=true",
+                 @"https://github.com/xiaohange/xiaohange.github.io/blob/master/images/csdnImg/test2.png?raw=true",
+                 @"http://cc.cocimg.com/api/uploads/20170809/1502261337401648.png",
+                 @"http://img4.07net01.com/upload/images/2017/03/25/55209250235451.gif",
+                 @"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502279410063&di=29202ef74ed4f488552a018c3455fba6&imgtype=0&src=http%3A%2F%2Fimg.zcool.cn%2Fcommunity%2F01db52582566a5a84a0d304fa0ecc7.jpg",
+                 @"https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1502279258970&di=0414b6665f00c42ed92a4f19d3424e0f&imgtype=0&src=http%3A%2F%2Fimg.361games.com%2Ffile%2Ftu%2Fmeinv%2Fqbkrf43nnig.jpg"];
     
     for (int i = 0; i<urlArray.count; i++) {
         UIImageView *imageView = [[UIImageView alloc] initWithFrame:CGRectMake(i%3 * 100 + (i%3 + 1)*(SCREENWIDTH-300)/4.0,100 + i/3 * 100 + (i/3 + 1)*(SCREENWIDTH-300)/4.0, 100, 100)];
